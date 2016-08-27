b704f627265064269b829d15e016ccb7752a4980
Pham Thanh <mrdark_9x@yahoo.com>
2016-08-27 14:41:24 +0700

    Initial Commit

---
+++ LoginForm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginForm
+//
+//  Created by TTB10912 on 8/27/16.
+//  Copyright © 2016 TTB10912. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LoginFormTests/LoginFormTests.swift
@@ -0,0 +1,36 @@
+//
+//  LoginFormTests.swift
+//  LoginFormTests
+//
+//  Created by TTB10912 on 8/27/16.
+//  Copyright © 2016 TTB10912. All rights reserved.
+//
+
+import XCTest
+@testable import LoginForm
+
+class LoginFormTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
